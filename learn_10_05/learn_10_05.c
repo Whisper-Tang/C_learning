b2e4fa65bb34561e3293c1f427e9dc0c7d47ccd2
Whisper-Tang <whispertangovo@outlook.com>
2024-10-05 00:07:06 +0800

    练习题目 和算法题目总算是轮到图了QAQ

---
+++ learn_10_05/learn_10_05.c
@@ -0,0 +1,6 @@
+#define _CRT_SECURE_NO_WARNINGS //让scanf函数可用不报错
+//#pragma warning(disable:4996)
+#pragma warning(disable:6031)//禁用警告提示:" C6031 返回值被忽略："scanf" "#
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
