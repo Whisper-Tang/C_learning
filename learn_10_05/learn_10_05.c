1409f3f8016b0d8fd95a1b2dc20ae5a6557b0fd8
Whisper-Tang <whispertangovo@outlook.com>
2024-10-06 03:17:42 +0800

    图的相关练习添加

--- learn_10_05/learn_10_05.c
+++ learn_10_05/learn_10_05.c
@@ -44,4 +44,78 @@ typedef struct ALGraph
 	int vexnum, arcnum;		//图中目前的顶点、边的个数
 }ALGraph;
 
+int main()
+{
+	return 0;
+}
 
+//邻接表转化为邻接矩阵
+//for (int i = 0; i < vexsum; i++)
+//{
+//	ArcNode* p = AdjList[i].firstArc;
+//	while (p)
+//	{
+//		Graph.arcs[i][p->adjvex] = p->info;
+//		p = p->nextarc;
+//	}
+//}
+
+
+//输出出度比入度大的结点k和返回k的数量
+int printVertices(AMGraph G)
+{
+	int num = 0;
+	for (int row = 0; row < G.vexnmu; row++)
+	{
+		int out = 0, in = 0;
+		for (int col = 0; col < G.vexnmu; col++)
+		{
+			if (G.arcs[row][col] != 0 || G.arcs[row][col] != MaxInt)		//求row行和
+				out++;
+			if (G.arcs[col][row] != 0 || G.arcs[col][row] != MaxInt)		//求row列和
+				in++;
+		}
+		if (out > in)	//行和大于列和，满足条件
+		{
+			printf("%c", G.vexs[row]);	//输出并计数加一
+			num++;
+		}
+			
+	}
+	return num;
+}
+
+
+//有向图中是否有顶点vi到vj的路径
+bool visited[MVNum];
+bool isRoad(ALGraph G, int i, int j)
+{
+	visited[i] = true;	// 标记顶点i已经访问
+	ArcNode* p = G.vertices[i].firstarc;	// 获取顶点i的第一条边
+
+	while(p)	// 遍历顶点i的所有邻接点
+	{
+		if (p->adjvex == j)	//i的第一个邻接点就是j
+			return true;
+		else if (visited[p->adjvex] == false)	//i的第一个邻接点不是j，且未被访问过
+			return isRoad(G, p->adjvex, j);	 // 递归检查从该邻接点到j是否有路径
+		p = p->nextarc;	// 检查下一个邻接点
+	}
+	return false;
+}
+
+//四种存储结构
+// 
+// 有向图无向图——邻接矩阵、邻接表
+// 十字链表（有向图），邻接多重表（无向图）
+
+//图的两种遍历方法：广度优先遍历BFS和深度优先遍历DFS(均为阵:O(n^2)和表:O(n+e))
+
+//图的应用
+// 
+//最小生成树：Prime算法、Kruskal算法（表:O(e log e)）
+// 
+//最短路径：	BFS（权值为1）	Dijkstra（无负权值）	Floyd（无带负权值的环）
+//时间复杂度		O(n^2)			O(n^2)				O(n^3)
+// 
+//拓扑排序——有向无环图，关键路径(均为阵:O(n^2)和表:O(n+e))
